c58926ec5c7de12b453969c6d6c48a1db6058db0
Nathan Hubens <nathan.hubens@gmail.com>
2020-12-21 15:12:31 +0100

    doc

--- fasterai_t/_nbdev.py
+++ fasterai_t/_nbdev.py
@@ -2,9 +2,9 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"say_hello": "00_core.ipynb"}
 
-modules = []
+modules = ["core.py"]
 
 doc_url = "https://nathanhubens.github.io/fasterai_t/"
 
